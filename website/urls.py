082cdf5c65cad02791c1bcbdc6fbacb0ae2439c6
mehdi-msv <163362438+mehdi-msv@users.noreply.github.com>
2024-03-22 02:39:16 +0330

    Convert text from static to dynamic

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import *
-
+app_name = 'website'
 urlpatterns = [
     path('',index,name='index')
 ]
--- website/views.py
+++ website/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 def index(request):
-    return render(request,'index.html')
+    context = {'NAME':'Mehdi',
+               'LAST_NAME':'Mousavi',
+               'Profile':'Backend Developer',
+               'Email':'mehdi.mousavi.rad1@gmail.com',
+               'Phone':'(+98) 916-854-3458'} 
+    return render(request,'index.html', context)
